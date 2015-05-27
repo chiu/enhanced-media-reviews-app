e35b5ddc21461a3bde6e33f33ab11cce5692305f
chiu <chiucode@gmail.com>
2015-05-27 21:10:13 +0000

    finished admin page with authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-
-  def index
-  @users = User.all
-end
-
+  
 
   helper_method :current_user
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527190621) do
+ActiveRecord::Schema.define(version: 20150527204930) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150527190621) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "admin"
   end
 
 end
