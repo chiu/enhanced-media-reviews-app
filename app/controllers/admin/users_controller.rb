f459bb670e198458acb31c30a93143c3727a4f4c
chiu <chiucode@gmail.com>
2015-05-29 20:54:24 +0000

    added edit, delete, admin status for admin user edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -26,8 +26,31 @@ def edit
 end
 
 
+def update
+
+  @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    redirect_to admin_users_path(@user)
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+  redirect_to admin_users_path
+end
+
+
+
 protected
-¡¡¡
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
+
+
 
 def authorize_admin
   if current_user.admin
